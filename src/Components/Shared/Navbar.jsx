7f75b88581967badf850ba5004748900d616fd28
Rifat Rabbi <rifayrabby78@gmail.com>
2023-10-18 00:16:06 +0600

    create successfully the add page form to add product

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   );
   return (
     <div>
-      <div className="navbar bg-lime-300">
+      <div className="navbar bg-blue-700 text-white">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
